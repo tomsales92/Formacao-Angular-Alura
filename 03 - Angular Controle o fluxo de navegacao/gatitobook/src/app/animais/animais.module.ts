f30928ae20d84c32d62207668a3b5909947e3de9
Everton Sales <everton.goncalves92@gmail.com>
2021-07-14 20:48:40 -0300

    aula curso 3 e doacoes

--- 03 - Angular Controle o fluxo de navegacao/gatitobook/src/app/animais/animais.module.ts
+++ 03 - Angular Controle o fluxo de navegacao/gatitobook/src/app/animais/animais.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
+import { CartaoModule } from '../componentes/cartao/cartao.module';
 
 
 @NgModule({
   declarations: [ListaAnimaisComponent, AnimalComponent],
   imports: [
     CommonModule,
-    AnimaisRoutingModule
+    AnimaisRoutingModule,
+    CartaoModule
   ]
 })
 export class AnimaisModule { }
